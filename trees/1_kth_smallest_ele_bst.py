e0b09f6145c6b09da41b0edb50bcb7c1f6b92ba9
Naveen Basavaraj <naveen.b.workmail@gmail.com>
2023-12-27 16:07:28 +0530

    tree

---
+++ trees/1_kth_smallest_ele_bst.py
@@ -0,0 +1,15 @@
+from binary_tree import TreeNode
+
+def kthSmallest(root, k):
+    stack = []
+    
+    curr = root
+    while stack or curr:
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+        curr = stack.pop()
+        k -= 1
+        if k == 0:
+            return curr.val
+        curr = curr.right
